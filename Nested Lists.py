d4f931068df10bff0b1c4501d27e87ce1181bf9e
Thushanths09 <thushanths09@gmail.com>
2022-03-20 20:41:20 +0530

    te

---
+++ Nested Lists.py
@@ -0,0 +1,6 @@
+a = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][1])
+        break
+        

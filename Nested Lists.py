bb078ce8601d0da34087fe9e1a8ebc2fdcc6d5ab
Thushanths09 <thushanths09@gmail.com>
2022-03-21 11:37:27 +0530

    nested

--- Nested Lists.py
+++ Nested Lists.py
@@ -1,6 +1,17 @@
-a = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        print(a[i][1])
-        break
+N=int(input("Enter Student Numbers: "))
+b=[]
+if __name__ == '__main__':
+    for i in range(N):
+        name = input("enter your name: ")
+        score = float(input("Enter Score: "))
+        a=[name,score]
+        b.append(a)
+c=[]
+for j in b:
+    c.append(j[1])
+print(b[c.index(min(c))][0])
+
+    
+
+
         

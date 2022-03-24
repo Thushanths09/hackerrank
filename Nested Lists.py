301ea58e726f061deb9bd1f96a2573ca6ce0bd99
Thushanth Sivashanthan <thushanths09@gmail.com>
2022-03-24 08:52:50 +0530

    Nested List

--- Nested Lists.py
+++ Nested Lists.py
@@ -1,17 +1,26 @@
-N=int(input("Enter Student Numbers: "))
-b=[]
 if __name__ == '__main__':
-    for i in range(N):
-        name = input("enter your name: ")
-        score = float(input("Enter Score: "))
-        a=[name,score]
-        b.append(a)
-c=[]
-for j in b:
-    c.append(j[1])
-print(b[c.index(min(c))][0])
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name,score]) #Create the nested list
+    x = 99999
+    for i in range(len(students)):
+        if x > float(students[i][1]):
+            x = float(students[i][1])
+    y = 999999
+    for i in range(len(students)):
+        if float(students[i][1]) > float(x) and y > float(students[i][1]):
+            y = float(students[i][1])
+    runner = []
+    for i in range(len(students)):
+        if float(students[i][1]) == float(y):
+            runner.append(students[i][0])
+    runner = sorted(runner)
 
-    
-
-
+    for i in range(len(runner)):
+        print(runner[i])
+        
+        
+        
         
---
+++ ai.py
@@ -0,0 +1,8 @@
+import math
+import datetime
+import turtle
+import os
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("Hello World")
